1cdeb8367c5703a125298587292b3bfa64067596
Pedro Caetano <pedrocaetanogons@gmail.com>
2020-03-12 11:34:44 -0300

    AudioManager play effect

--- Assets/Scripts/NavGame/Manager/AudioManager.cs
+++ Assets/Scripts/NavGame/Manager/AudioManager.cs
@@ -3,19 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace NavGame.Managers
-{
-public class AudioManager : MonoBehaviour
-{
-public Clip[] clips;
+namespace NavGame.Managers {
+    public class AudioManager : MonoBehaviour {
+        public static AudioManager instance;
+        public Clip[] clips;
 
+        Dictionary<string, AudioClip> dict = new Dictionary<string, AudioClip> ();
+        void Awake () {
+            if (instance == null) {
+                instance = this;
 
+            } else {
+                Destroy (gameObject);
+            }
+        }
 
-[Serializable]
-    public class Clip
-    {
-        public string name;
-        public AudioClip audio;
+        void Start () {
+            foreach (Clip clip in clips) {
+                dict.Add (clip.name, clip.audio);
+
+            }
+        }
+
+        public void Play (string clipName, Vector3 position) {
+            if (dict.ContainsKey (clipName)) {
+                AudioSource.PlayClipAtPoint (dict[clipName], position);
+
+            }
+        }
+
+        [Serializable]
+        public class Clip {
+            public string name;
+            public AudioClip audio;
+        }
+
     }
-}
 }
