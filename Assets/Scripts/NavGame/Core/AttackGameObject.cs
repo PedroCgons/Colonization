10d38b6afdbd61386726c44a289d789853ca4e24
Pedro Caetano <pedrocaetanogons@gmail.com>
2020-06-04 10:37:06 -0300

    Saving enemies in aggro range

--- Assets/Scripts/NavGame/Core/AttackGameObject.cs
+++ Assets/Scripts/NavGame/Core/AttackGameObject.cs
@@ -6,50 +6,68 @@ using NavGame.Managers;
 
 namespace NavGame.Core
 {
-[RequireComponent(typeof(NavMeshAgent))]
-public class AttackGameObject : TouchableGameObject
-{
-    public OfenseStats ofenseStats;
-      protected NavMeshAgent agent;
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class AttackGameObject : TouchableGameObject
+    {
+        public OfenseStats ofenseStats;
+
+        public string[] enemyLayers;
+        [SerializeField]
+        protected List<DamageableGameObject> enemiesToAttack = new List<DamageableGameObject>();
+        protected NavMeshAgent agent;
         float cooldown = 0f;
+        LayerMask enemyMask;
 
-    public OnAttackHitEvent onAttackHit;
+        public OnAttackHitEvent onAttackHit;
 
-    protected virtual void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
+        protected virtual void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            enemyMask = LayerMask.GetMask(enemyLayers);
+        }
+
+        protected virtual void Update()
+        {
+            DecreaseAttackCooldown();
+        }
+        public void AttackOnCooldown(DamageableGameObject target)
+        {
+            if (cooldown <= 0f)
+            {
+                cooldown = 1f / ofenseStats.attackSpeed;
+                target.TakeDamage(ofenseStats.damage);
+                if (onAttackHit != null)
+                {
+                    onAttackHit(target.transform.position);
+                }
+
+            }
+
+        }
+
+        void DecreaseAttackCooldown()
+        {
+            if (cooldown == 0f)
+            {
+                return;
+            }
+            cooldown = cooldown - Time.deltaTime;
+            if (cooldown < 0f)
+            {
+                cooldown = 0f;
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (enemyMask.Contains(other.gameObject.layer))
+            {
+                DamageableGameObject obj = other.transform.parent.GetComponent<DamageableGameObject>();
+                if (!enemiesToAttack.Contains(obj))
+                {
+                    enemiesToAttack.Add(obj);
+            }
+            }
+        }
     }
-    
-    protected virtual void Update()
-    {
-        DecreaseAttackCooldown();
-    }
-   public void AttackOnCooldown(DamageableGameObject target)
-   {
-       if (cooldown <= 0f)
-       {
-           cooldown = 1f / ofenseStats.attackSpeed;
-           target.TakeDamage(ofenseStats.damage);
-           if (onAttackHit != null)
-           {
-               onAttackHit(target.transform.position);
-           }
-           
-       }
-
-   }
-
-   void DecreaseAttackCooldown()
-   {
-       if (cooldown == 0f)
-       {
-           return;
-       }
-       cooldown = cooldown - Time.deltaTime;
-       if (cooldown < 0f)
-       {
-           cooldown = 0f;
-       }
-   }
-}
 }
