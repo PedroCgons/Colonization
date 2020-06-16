cb2cdb5f399086ab97a15adf076a70bbce482acf
Pedro Caetano <pedrocaetanogons@gmail.com>
2020-06-16 19:20:13 -0300

    Damageable sounds

--- Assets/Scripts/Characters/CreepController.cs
+++ Assets/Scripts/Characters/CreepController.cs
@@ -20,7 +20,6 @@ public class CreepController : AttackGameObject
             finalTarget = obj.GetComponent<DamageableGameObject>();
 
         }
-        onAttackHit += PlayEffects;
     }
     protected override void Update()
     {
@@ -45,8 +44,5 @@ public class CreepController : AttackGameObject
             agent.SetDestination(finalTarget.transform.position);
         }
     }
-    void PlayEffects(Vector3 position)
-    {
-        AudioManager.instance.Play("enemy-hit", position);
-    }
+ 
 }
